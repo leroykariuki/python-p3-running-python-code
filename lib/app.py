4d08e4af90d3b81de60296e09fce46fe3091e066
leroy <leroy.kariuki@student.moringaschool.com>
2023-11-20 21:13:28 +0300

    done

--- lib/app.py
+++ lib/app.py
@@ -1,3 +1,10 @@
 # lib/app.py
-print("Hello world!", end=" ")
-print("Pass this test,", end="please.")
+def filter_moringa_names(names):
+    moringa_students = ("Alice", "David", "Eve")
+    moringa_students_list = (name for name in names if name in moringa_students)
+    return moringa_students_list
+
+names_list = ("Alice", "Eve", "John", "David", "Jane")
+
+moringa_students_list = filter_moringa_names(names_list)
+print("Moringa School Students:", moringa_students_list)
